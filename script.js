c9d9460bfdd873a95cc1b840800e70bea0303509
Prathmesh Khandelwal <56167998+prathmeshkhandelwal1@users.noreply.github.com>
2019-12-23 01:57:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var listItems = document.getElementsByTagName("li");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	li.className="list-group-item list-group-item-dark";
+	
+	var btn= document.createElement("button");
+	btn.appendChild(document.createTextNode("Delete!"));
+	li.appendChild(btn);
+	btn.onclick=removeParent;
+	btn.className="btn btn-dark padding";
+	
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.onclick=function(event){
+	var target = event.target;
+	target.classList.toggle("done");
+}
+function listLength(){
+	 return listItems.length;
+}
+// function deleteButton(){
+// 	var btn= document.createElement("button");
+// 	btn.appendChild(document.createTextNode("Delete!"));
+// 	listItems[i].appendChild(btn);
+// 	btn.onclick=removeParent;
+// }
+
+// for( i=0;i<listLength;i++)
+// {
+// 	deleteButton();
+// }
+function removeParent(evt){
+	evt.target.parentNode.remove();
+}
